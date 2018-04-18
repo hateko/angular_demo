cae89b9ad15f0157d8e3997c6b6e714668203eb3
mlh <mlh3169@gmail.com>
2018-04-18 18:43:17 +0800

    $watch & $digest

--- src/hello.js
+++ src/hello.js
@@ -1,3 +1,5 @@
-module.exports = function sayHello() {
-  return 'hello';
+var _ = require('lodash');
+
+module.exports = function sayHello(to) {
+  return _.template('Hello, <%= name%>!')({name: to})
 };
--- test/hello_spec.js
+++ test/hello_spec.js
@@ -2,6 +2,6 @@ var sayHello = require('../src/hello');
 
 describe('Hello', function(){
   it('says hello', function() {
-    expect(sayHello()).toBe('hello');
+    expect(sayHello('Jane')).toBe('Hello, Jane!');
   });
 });
